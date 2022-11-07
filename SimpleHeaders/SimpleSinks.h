1c34872fbe8024019931b5b1b74d4cf21453f596
TylerSeanRau <the.real.tyler.rau@gmail.com>
2022-11-06 18:44:37 -0800

    Add SimpleSinkTurnOffLight
    Added a new sink named SimpleSinkTurnOffLight this sink will send
    a LOW message to pin 5 on the RaspberryPi's 3B+ GPIO connector.
    This is the required messeage to the ReSpeaker 4Mic LED array
    to turn off the LED lights.

--- SimpleHeaders/SimpleSinks.h
+++ SimpleHeaders/SimpleSinks.h
@@ -8,6 +8,8 @@
 #include "SimpleAudioVerifier.h"
 #include "SimplePartialHandler.h"
 
+#include <wiringPi.h>
+
 class SimpleSinkWithoutClosingSound : public ClientCapabilityRegistry::DataSink
 {
   protected:
@@ -39,4 +41,32 @@ class SimpleSinkWithoutClosingSound : public ClientCapabilityRegistry::DataSink
       }
 };
 
+class SimpleSinkTurnOffLight : public SimpleSinkWithoutClosingSound
+{
+  public:
+    SimpleSinkTurnOffLight(
+      HoundConverser::VoiceRequest *voice_request,
+      SimplePartialHandler * partial_handler
+    )
+      :
+        SimpleSinkWithoutClosingSound(
+          voice_request,
+          partial_handler
+        )
+    {
+    }
+
+    ~SimpleSinkTurnOffLight(void)  { }
+
+    virtual size_t write_data(const uint8_t *data, size_t byte_count){
+      if((!(voice_request->more_ok()))||partial_handler->getServerSaysStop()){
+        digitalWrite(5,LOW);
+        return 0;
+      }
+
+      verifier.add_bytes(&(data[0]), byte_count);
+      voice_request->add_audio(byte_count, data);
+      return byte_count;
+    }
+};
 #endif /* SIMPLESINKS_H */
