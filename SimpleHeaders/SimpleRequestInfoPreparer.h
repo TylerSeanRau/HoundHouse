b6ab5f4d13916fb8cb2ef394a7338123560cb9ca
TylerSeanRau <the.real.tyler.rau@gmail.com>
2022-11-06 18:44:37 -0800

    Add Lat/Long to Simple Request Info preparer
    Added the ability to pass a latitude and longitude to the request
    info.  Tons of queries, like "what's the weather like" depend
    on the users current location. This will allow users of this
    class to pass that information.

--- SimpleHeaders/SimpleRequestInfoPreparer.h
+++ SimpleHeaders/SimpleRequestInfoPreparer.h
@@ -12,9 +12,45 @@
 
 class SimpleRequestInfoPreparer : public RequestInfoPreparer
 {
-  private:
+  protected:
+    const bool has_lat_to_use;
+    const double lat_to_use;
+    const bool has_lon_to_use;
+    const double lon_to_use;
+
   public:
     SimpleRequestInfoPreparer(void)
+      :
+        has_lat_to_use(false),
+        lat_to_use(0.0),
+        has_lon_to_use(false),
+        lon_to_use(0.0)
+    {
+    }
+
+    SimpleRequestInfoPreparer(
+      const double lat,
+      const double lon
+    )
+      :
+        has_lat_to_use(true),
+        lat_to_use(lat),
+        has_lon_to_use(true),
+        lon_to_use(lon)
+    {
+    }
+
+    SimpleRequestInfoPreparer(
+      const bool lat_passed,
+      const double lat,
+      const bool lon_passed,
+      const double lon
+    )
+      :
+        has_lat_to_use(lat_passed),
+        lat_to_use(lat),
+        has_lon_to_use(lon_passed),
+        lon_to_use(lon)
     {
     }
 
@@ -29,6 +65,20 @@ class SimpleRequestInfoPreparer : public RequestInfoPreparer
 #endif /* HOUNDHOUSEDEBUG */
 
       request_info->setServerDeterminesEndOfAudio(true);
+
+      if(has_lat_to_use){
+        request_info->setLatitude(lat_to_use);
+#ifdef HOUNDHOUSEDEBUG
+        std::cout<<"lat: "<<lat_to_use<<std::endl;
+#endif /* HOUNDHOUSEDEBUG */
+      }
+
+      if(has_lon_to_use){
+        request_info->setLongitude(lon_to_use);
+#ifdef HOUNDHOUSEDEBUG
+        std::cout<<"lon: "<<lon_to_use<<std::endl;
+#endif /* HOUNDHOUSEDEBUG */
+      }
 
 #ifdef HOUNDHOUSEDEBUG
       std::cout<<"Finished RequestInfoJSON preperations."<<std::endl;
